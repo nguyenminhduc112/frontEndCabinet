520368d5a03ec0dd8cd47db505d8fc4ba1c1644a
kira-mobile <102649919+kira-mobile@users.noreply.github.com>
2022-07-12 16:14:43 +0700

    Header no dropDown

--- src/pages/adminPage/components/Header/Header.js
+++ src/pages/adminPage/components/Header/Header.js
@@ -2,23 +2,29 @@ import classNames from "classnames/bind";
 import styles from './Header.module.scss';
 import images from "../../../../assets/images";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-
+import { faAngleDown, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom';
+import config from '../../../../config'
 
 const cx = classNames.bind(styles)
 
 function Header() {
+    const info = {
+        icon: <FontAwesomeIcon icon={faUser} />,
+        title: 'LHU B304 LAB',
+        dropDownIcon: <FontAwesomeIcon icon={faAngleDown} />,
+    }
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('logo-wrapper')}>
+            <Link className={cx('logo-wrapper')} to={config.routes.dashboard}>
                 <img src={images.logo} alt='Logo' className={cx('logo')} />
-            </div>
+            </Link>
             <div className={cx('info-wrapper')}>
-                <span>
-                    <FontAwesomeIcon icon={faUser} />
-                </span>
+                <span className={cx('icon')} >{info.icon}</span>
+                <span className={cx('title')}>{info.title}</span>
+                <span className={cx('iconDrop')}>{info.dropDownIcon}</span>
             </div>
-        </header>
+        </header >
     );
 }
 
