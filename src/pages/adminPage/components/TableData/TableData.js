73338b22e351c6910d007999108e2630c7364e75
kira-mobile <102649919+kira-mobile@users.noreply.github.com>
2022-07-12 20:05:47 +0700

    Xong giao diện Dashboard Page

---
+++ src/pages/adminPage/components/TableData/TableData.js
@@ -0,0 +1,55 @@
+import classNames from "classnames/bind";
+import styles from './TableData.module.scss';
+
+const cx = classNames.bind(styles)
+
+function TableData({ tblDataTitles, components, className, }) {
+
+
+
+    return (
+        <div className={cx('wrapper')}>
+            <table className={cx('table')}>
+                <thead className={cx('heading')}>
+                    <tr>
+                        {tblDataTitles.map(tblDataTitle => (
+                            <th>{tblDataTitle}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {components.map(component => (
+                        <tr>
+                            <td>
+                                {component.device}
+                            </td>
+                            <td>
+                                {component.quantity}
+                            </td>
+                            <td>
+                                {component.time}
+                            </td>
+                            <td>
+                                {component.name}
+                            </td>
+
+                            {!component.state ? (
+                                <td className={cx('red')}>
+                                    Chưa trả
+                                </td>
+                            ) : (
+                                <td className={cx('green')}>
+                                    Đã trả
+                                </td>
+                            )}
+
+                        </tr>
+                    ))}
+                </tbody>
+
+            </table>
+        </div>
+    );
+}
+
+export default TableData;
--- src/pages/adminPage/pages/Home/Body/Body.js
+++ src/pages/adminPage/pages/Home/Body/Body.js
@@ -1,12 +1,48 @@
 import classNames from "classnames/bind";
 import styles from './Body.module.scss';
+import TableData from "../../../components/TableData";
 
 const cx = classNames.bind(styles)
 
 function Body() {
+    const tblDatas = {
+        // Vì chưa có API nên tạm thời gắn cứng datas
+        title: [
+            'Tên thiết bị',
+            'Số lượng mượn',
+            'Thời gian mượn',
+            'Vị trí',
+            'Trạng thái',
+        ],
+
+        datas: [
+            {
+                device: 'MacBook Air 1',
+                quantity: 2,
+                time: '9:30 AM - 18/04/2003',
+                name: 'Tủ 1',
+                state: true,
+            },
+            {
+                device: 'MacBook Air 2',
+                quantity: 3,
+                time: '9:30 AM - 19/04/2003',
+                name: 'Tủ 1',
+                state: false,
+            }
+        ]
+
+    }
+
+
     return (
         <div className={cx('wrapper')}>
-            <h2>Body</h2>
+            <div className={cx('header')}>Danh sách thống kê</div>
+            <TableData
+                tblDataTitles={tblDatas.title}
+                components={tblDatas.datas}
+                key={tblDatas.title}
+            />
         </div>
     );
 }
